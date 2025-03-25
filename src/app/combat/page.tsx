d47e7bd9b76cbd84b1a3aab2cfec1bf2e9244d48
Amanda Hallstedt <hallsted@kth.se>
2025-03-25 10:31:54 +0100

    CombatOverview page and component

---
+++ src/app/combat/page.tsx
@@ -0,0 +1,8 @@
+import CombatOverview from "@/components/CombatOverview";
+
+export default function CombatPage() {
+  return (
+      <CombatOverview />
+      // Maybe later make more components for combat, or create two different views? One for DM, one for players
+  );
+}
---
+++ src/components/CombatOverview.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import { inknutAntiqua } from "@/lib/fonts";
+
+
+const CombatOverview: React.FC = () => {
+
+  return (
+    <div className={`flex flex-row justify-center gap-4 pt-15 ${inknutAntiqua.className} text-[15px]`}>
+      Öhhhhhh hej
+    </div>
+  );
+};
+
+export default CombatOverview;
