a5dd5c5d172b927b80a464163e2176a465276804
Amanda Hallstedt <hallsted@kth.se>
2025-03-25 10:34:44 +0100

    Commented plan for combat overview

--- src/components/CombatOverview.tsx
+++ src/components/CombatOverview.tsx
@@ -4,11 +4,16 @@ import React, { useState, useEffect } from "react";
 import { inknutAntiqua } from "@/lib/fonts";
 
 
+// Start/end button (reset?)
+// On start -> Pick players, add allies/enemies (choose HP/name) -> roll for order (initiative for each) -> place them in order
+// In combat -> Note active player -> Place damage, note actions/spell slots, note downed/dead
+// End -> go back to default, save/cache player HP 
+
 const CombatOverview: React.FC = () => {
 
   return (
     <div className={`flex flex-row justify-center gap-4 pt-15 ${inknutAntiqua.className} text-[15px]`}>
-      Öhhhhhh hej
+      
     </div>
   );
 };
